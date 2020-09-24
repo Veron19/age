c6f6611d807b3d6f10c3570f4c6082399a2b8507
Veron19 <kravchenkoveron@gmail.com>
2020-09-24 23:24:49 +0300

    start file

---
+++ calculator.py
@@ -0,0 +1,13 @@
+what = input ("Что делаем? (+,-) ")
+a = input ("Введите 1 число:")
+b = input ("Введите 2 число:")
+if what == "+":
+    c = int(a) + int(b)
+    print ("Результат:" + str(c))
+elif what == "-":
+     c = int (a) - int (b)
+     print ("Результат:" + str (c))
+# else a == float input ("Введите 1 число:")
+
+
+
